5791d13061a43391d3a9668741709b4ad5525d25
Amy Richardson <amyrichardson96@gmail.com>
2018-01-16 15:31:45 -0600

    display all pets on page load

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -14,11 +14,29 @@ function getPets() {
     $.ajax({
         method: 'GET',
         url: '/pets',
-        success: function(response){
-            console.log('success!:', response);
-        } //end success
+        success: displayPets
     }); //end ajax get
 } // End getPets
+
+function displayPets(pets) {
+    console.log('pets: ', pets);
+    
+    for (let i = 0; i < pets.length; i++) {
+        let pet = pets[i]
+        let $row = $(`<tr data-id="${pet.id}">`);
+        $row.append(`
+        <td>${pet.first_name} ${pet.last_name}</td>
+        <td>${pet.name}</td>
+        <td>${pet.breed}</td>
+        <td>${pet.color}</td>
+        <td><button class="edit-pet">Edit</button></td>
+        <td><button class="delete-pet">Delete</button></td>
+        <td><button class="check-in-out">Check In/Out</button></td>
+        `);
+        $('.pet-list').append($row);
+    } //end for loop
+
+} //end displayPets
 
 function addPets() {
 } // End addPets

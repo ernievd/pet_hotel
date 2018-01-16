4c8ed8f058acf2b58bc4c480d15b31329b39fcd6
Ross <ross@tests-MacBook-Pro.local>
2018-01-16 15:30:32 -0600

    router complete for PUT

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-
+router.put('/:id', (req, res) => {
+    const queryText = `UPDATE pet SET name=$1, breed=$2, color=$3 WHERE id=$4`;
+    pool.query(queryText, [req.body.name, req.body.breed, req.body.color, req.params.id])
+    .then((result) => {
+        console.log('Edit complete: ', req.params.id);
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('Error Editing.', error);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = router;

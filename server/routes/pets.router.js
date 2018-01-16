4b5d9b53d1353445983d1252bad18280e893a3ec
ernievd <ernievd@gmail.com>
2018-01-16 16:37:16 -0600

    Additions added for register new pet

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -18,7 +18,6 @@ router.get('/', (req, res) => {
 
 
 router.post('/', function(req, res) {
-    console.log('req.body is', req.body)
     const queryText = 'INSERT INTO owner (first_name, last_name) VALUES($1, $2)';
     pool.query(queryText, [req.body.firstName, req.body.lastName])
     // runs on successful query
@@ -33,7 +32,21 @@ router.post('/', function(req, res) {
         });
 });// End router.post
 
-
+router.post('/newPet', function(req, res) {
+    console.log('req.body of /newPet is', req.body);
+    const queryText = 'INSERT INTO pet (name, breed, color) VALUES($1, $2, $3)';
+    pool.query(queryText, [req.body.name, req.body.breed, req.body.color])
+    // runs on successful query
+        .then((result) => {
+            //console.log('query results: ', result);
+            res.sendStatus(201);
+        })
+        // error handling
+        .catch((err) => {
+            console.log('error posting to owner table:', err);
+            res.sendStatus(500);
+        });
+});// End router.post
 
 router.put('/:id/:boolean', (req, res)=>{
     console.log(req.params.id, req.params.boolean);
